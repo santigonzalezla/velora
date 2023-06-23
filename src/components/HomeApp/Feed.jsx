d1c67cb34ed1214f1c8c1593f64a421bb6b40eaa
dianamorenodev <valentinamoreno2002@gmail.com>
2023-06-23 01:24:28 -0500

    Update Card and side bar

--- src/components/HomeApp/Feed.jsx
+++ src/components/HomeApp/Feed.jsx
@@ -2,6 +2,9 @@ import style from './HomeApp.module.css'
 import LogoSide from '../../assets/img/HomeApp/Feed/logoside.png'
 import userPhoto from '../../assets/img/HomeApp/Feed/userPhoto.png'
 import postImg from '../../assets/img/HomeApp/Feed/postImg.png'
+import commentReaction from '../../assets/img/HomeApp/Feed/commentReaction.png'
+import heartReaction from '../../assets/img/HomeApp/Feed/heartReaction.png'
+import donationReaction from '../../assets/img/HomeApp/Feed/donationReaction.png'
 
 const Feed = () => {
   return (
@@ -20,10 +23,35 @@ const Feed = () => {
         <div className={style.PostContent}>
           <img src={postImg} alt="" className={style.PostImg} />
           <div className={style.PostReaction}>
-
+            <img src={commentReaction} alt="" className={style.iconsReaction} />
+            <img src={heartReaction} alt="" className={style.iconsReaction} />
+            <img src={donationReaction} alt="" className={style.iconsReaction} />
           </div>
-          <p className={style.postTxt}>Lorem ipsum dolor sit amet, Cum sociis natoque penatibus et magnis dis parturient. Purus faucibus ornare suspendisse sed nisi lacus sed. </p>
-          <p>View more...</p>
+          <div>
+            <p className={style.postTxt}>Lorem ipsum dolor sit amet, Cum sociis natoque penatibus et magnis dis parturient. Purus faucibus ornare suspendisse sed nisi lacus sed. </p>
+            <p className={style.postViewMore}>View more...</p>
+          </div>
+        </div>
+      </div>
+      <div className={style.Post}>
+        <div className={style.porfile}>
+          <img src={userPhoto} alt="" className={style.userPhoto} />
+          <div className={style.userData}>
+            <span className={style.userNick}>Beautyful Mouse</span>
+            <span>@beautyfulmuse112</span>
+          </div>
+        </div>
+        <div className={style.PostContent}>
+          <img src={postImg} alt="" className={style.PostImg} />
+          <div className={style.PostReaction}>
+            <img src={commentReaction} alt="" className={style.iconsReaction} />
+            <img src={heartReaction} alt="" className={style.iconsReaction} />
+            <img src={donationReaction} alt="" className={style.iconsReaction} />
+          </div>
+          <div>
+            <p className={style.postTxt}>Lorem ipsum dolor sit amet, Cum sociis natoque penatibus et magnis dis parturient. Purus faucibus ornare suspendisse sed nisi lacus sed. </p>
+            <p className={style.postViewMore}>View more...</p>
+          </div>
         </div>
       </div>
     </main>
