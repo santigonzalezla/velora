60d264fcedb1adaa4d56ebe680da176ebd1192b9
dianamorenodev <valentinamoreno2002@gmail.com>
2023-08-15 18:07:20 -0500

    Update billing

--- src/components/CompanyApp/BillingCompany.jsx
+++ src/components/CompanyApp/BillingCompany.jsx
@@ -1,9 +1,19 @@
 import Style from './CompanyApp.module.css'
+import SideBarCompany from './SideBarCompany';
+import { Row, Col, Container } from "react-bootstrap";
+import Logo from '../../assets/img/CompanyApp/Billing/Logo.png'
+
 
 const BillingCompany = () => {
   return (
-    <div>
-      <h1>Billings</h1>
+    <div id={Style.CompanyApp}>
+      <SideBarCompany />
+      <Container id={Style.CompanyAppContainer}>
+        <section id={Style.Billinghead}>
+          <h3>Billings</h3>
+          <img src={Logo} alt="" width={400} />
+        </section>
+      </Container>
     </div>
   );
 }
