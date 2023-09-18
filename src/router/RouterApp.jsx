166b19ec152db277a95092953f053e12df968f46
NYmmip <57047465+NYmmip@users.noreply.github.com>
2023-09-18 02:26:31 -0500

    Merge

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -13,6 +13,55 @@ import ArtistDashboard from '../components/ArtistDashboard/ArtistDashboard'
 import VideoCallArtist from '../components/VideoCallArtist/VideoCallArtist'
 import LivesArtist from '../components/LivesArtist/LivesArtist'
 import BillingArtist from '../components/BillingArtist/BillingArtist'
+import HomeApp from '../components/HomeApp/HomeApp'
+import ViewArtist from '../components/ViewArtist/ViewArtist';
+import Chats from '../components/Chats/Chats'
+import Lives from '../components/Lives/Lives';
+import Subscriptions from '../components/Subscriptions/Subscriptions';
+import Billing from '../components/Billing/Billing';
+import VideoCall from '../components/VideoCall/VideoCall';
+// import ClientProtectedRoute from '../utils/clientProtectedRoute';
+
+// const home = () => {
+//   return (
+//     <ClientProtectedRoute>
+//       <HomeApp />
+//     </ClientProtectedRoute>
+//   )
+// }
+
+// const chats = () => {
+//   return (
+//     <ClientProtectedRoute>
+//       <Chats />
+//     </ClientProtectedRoute>
+//   )
+// }
+
+// const subscriptions = () => {
+//   return (
+//     <ClientProtectedRoute>
+//       <Subscriptions />
+//     </ClientProtectedRoute>
+//   )
+// }
+
+// const billing = () => {
+//   return (
+//     <ClientProtectedRoute>
+//       <Billing />
+//     </ClientProtectedRoute>
+//   )
+// }
+
+// const videoCall = () => {
+//   return (
+//     <ClientProtectedRoute>
+//       <VideoCall />
+//     </ClientProtectedRoute>
+//   )
+// }
+
 
 const RouterApp = () => {
   return (
@@ -31,6 +80,13 @@ const RouterApp = () => {
       <Route path='/VideoCallArtist' element={<VideoCallArtist />} />
       <Route path='/LivesArtist' element={<LivesArtist />} />
       <Route path='/BillingArtist' element={<BillingArtist />} />
+      <Route path='/home' element={<HomeApp />}/*element={home()}*/ />
+      <Route path='/ViewArtist' element={<ViewArtist />} />
+      <Route path='/Chats' element={<Chats />}/*element={chats()}*/ />
+      <Route path='/Lives' element={<Lives />} />
+      <Route path='/Subscriptions' element={<Subscriptions />}/*element={subscriptions()} */ />
+      <Route path='/Billing' element={<Billing />}/*element={billing()}*/ />
+      <Route path='/VideoCall' element={<VideoCall />}/* element={videoCall()}*/ />
     </Routes>
   );
 }
