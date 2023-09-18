a6684c8f34ca22952a5c8bf7da59253185d79b51
NYmmip <57047465+NYmmip@users.noreply.github.com>
2023-09-18 02:20:04 -0500

    Merge

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -5,6 +5,14 @@ import Artist from '../components/Artist/Artist'
 import Company from '../components/Company/Company'
 import Terms from '../components/Footer/Terms'
 import Help from '../components/Footer/Help';
+import ArtistApp from '../components/ArtistApp/ArtistApp';
+import ArtistPorfile from '../components/ArtistPorfile/ArtistPorfile'
+import ArtstChats from '../components/ArtistChats/ArtistChats'
+import ClientPorfile from '../components/ClientPorfile/ClientPorfile'
+import ArtistDashboard from '../components/ArtistDashboard/ArtistDashboard'
+import VideoCallArtist from '../components/VideoCallArtist/VideoCallArtist'
+import LivesArtist from '../components/LivesArtist/LivesArtist'
+import BillingArtist from '../components/BillingArtist/BillingArtist'
 
 const RouterApp = () => {
   return (
@@ -15,6 +23,14 @@ const RouterApp = () => {
       <Route path='/Company' element={<Company />} />
       <Route path='/Terms' element={<Terms />} />
       <Route path='/Help' element={<Help />} />
+      <Route path='/ArtistApp' element={<ArtistApp />} />
+      <Route path='/ArtstChats' element={<ArtstChats />} />
+      <Route path='/ArtistPorfile' element={<ArtistPorfile />} />
+      <Route path='/ClientPorfile' element={<ClientPorfile />} />
+      <Route path='/ArtistDashboard' element={<ArtistDashboard />} />
+      <Route path='/VideoCallArtist' element={<VideoCallArtist />} />
+      <Route path='/LivesArtist' element={<LivesArtist />} />
+      <Route path='/BillingArtist' element={<BillingArtist />} />
     </Routes>
   );
 }
