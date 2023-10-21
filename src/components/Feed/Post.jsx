11a18be12c8f10635e3235c3c9863cb8698a38dc
dianamorenodev <valentinamoreno2002@gmail.com>
2023-10-20 19:20:12 -0500

    Fix Post Bug

--- src/components/Feed/Post.jsx
+++ src/components/Feed/Post.jsx
@@ -47,10 +47,12 @@ const Post = (props) => {
             </Carousel>
             :
             <div>
-              {postImages[0][1] ? (postImages[0][1].split("/")[0] === "video" ?
-                <video src={postImages[0][0]} className={style.postImg} controls /> :
-                <Image src={postImages[0][0]} alt="" className={style.postImg} fluid />
-              ) : <></>}
+              {postImages.length > 0 ?
+                (postImages[0][1].split("/")[0] === "video" ?
+                  <video src={postImages[0][0]} className={style.postImg} controls /> :
+                  <Image src={postImages[0][0]} alt="" className={style.postImg} fluid />
+                ) : <>  </>
+              }
             </div>
         }
 
